2f83f472b0bd34d85b15baec179cae09ea499fcd
sloanbgwaltney <sloanbgwaltney@gmail.com>
2019-08-25 00:04:41 -0400

    added libs and middleware

--- app.js
+++ app.js
@@ -3,11 +3,25 @@ require("dotenv").config();
 require("./config/mongoose");
 // -------------------------------------------
 
+// -------- Libs Import --------------------
 const express = require("express");
+const bodyParser = require("body-parser");
+// -----------------------------------------
 
+// -------- Routes Import ------------------
+const usersRouter = require("./routes/user");
+// -----------------------------------------
 const app = express();
+
+// ----------Pre Route Middleware ----------
+app.use(bodyParser.json())
+//------------------------------------------
+
+// ----------Routers -----------------------
+app.use("/users", usersRouter.router)
+// -----------------------------------------
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Form Manager API active on port: ${port}`)
+    console.log(`Form Manager API active on port: ${port}`);
 })
